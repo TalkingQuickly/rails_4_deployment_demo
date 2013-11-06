abde6e817875358deb38ac5e5b1dca5da6b5166e
Ben Dixon <ben@hillsbede.co.uk>
2013-11-06 13:47:10 +0000

    capistrano installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultiAppsVps::Application.config.secret_key_base = '1e60fa128289ee7eb39b45a58bd34710daecc668f98b74f9b6ccbddb99e61ef590a2d6707ece8ca4e9fd93d001614f8a43b4de6d127f53d6ec605a45d4a6c876'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MultiAppsVps::Application.config.session_store :cookie_store, key: '_multi_apps_vps_session'
